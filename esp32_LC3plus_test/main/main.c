61ed7ccf88dec1aeb2a026bf247c053c4c9b9506
Kasper Nyhus Kaae <kanyhus@gmail.com>
2022-05-19 17:57:05 +0200

    zfdv

--- esp32_LC3plus_test/main/main.c
+++ esp32_LC3plus_test/main/main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include "esp_log.h"
 #include "esp_lc3plus.h"
+#include "sig_gen.h"
+
+#define SAMPLES_NUM 480
+#define BYTES_PER_SAMPLE 3
+#define BYTES_PER_FRAME ( SAMPLES_NUM * BYTES_PER_SAMPLE )
+
+sig_gen_t sigGenL;
+sig_gen_t sigGenR;
+int32_t samples[BYTES_PER_FRAME];
+uint8_t lc3binary[20000];
 
 
 void app_main(void)
 {
+    sig_gen_config_t cfg = {
+        .gen_source = SINE_LUT,
+        .lut_freq = LUT_FREQ_1K,
+        .bytes_per_sample = 3,
+        .sample_rate = 48000,
+        .endianess = SIG_GEN_LE
+    };
+    sig_gen_init(&sigGenL, &cfg);
+    sig_gen_init(&sigGenR, &cfg);
+
+    sig_gen_output_combine(&sigGenL, &sigGenR, samples, SAMPLES_NUM);
+
+    ESP_LOG_BUFFER_HEX("pre", samples, BYTES_PER_FRAME);
+
     lc3plus_init();
     
+    lc3plus_enc(samples, lc3binary);
+
+    ESP_LOG_BUFFER_HEX("post", lc3binary, BYTES_PER_FRAME);
 }
--- sig_gen_test/main/main.c
+++ sig_gen_test/main/main.c
@@ -9,9 +9,11 @@
 sig_gen_t sigGenL;
 sig_gen_t sigGenR;
 
-#define BYTES_PER_SAMPLE 4
+#define SAMPLES_NUM 4096
+#define BYTES_PER_SAMPLE 2
 
-#define BUF_SZ 480*2*BYTES_PER_SAMPLE
+
+#define BUF_SZ SAMPLES_NUM*BYTES_PER_SAMPLE*2
 uint8_t buffer[BUF_SZ];
 
 
@@ -20,20 +22,20 @@ void app_main(void)
     sig_gen_config_t sigGen_config = {
         // .gen_source = LUT_GEN,
         // .lut_freq = LUT_FREQ_1K,
-        .gen_source = CALC_GEN,
-        .freq = 2000,
-        .amplitude = 1,
-        .phase = 0,
+        .gen_source = WHITE_NOISE,
+        // .freq = 2000,
+        // .amplitude = 1,
+        // .phase = 0,
         .sample_rate = 48000,
         .bytes_per_sample = BYTES_PER_SAMPLE,
         .endianess = SIG_GEN_LE
     };
     sig_gen_init(&sigGenL,&sigGen_config);
     // sigGen_config.lut_freq = LUT_FREQ_5K3;
-    sigGen_config.freq = 329;
+    // sigGen_config.freq = 329;
     sig_gen_init(&sigGenR,&sigGen_config);
 
-    sig_gen_output_combine(&sigGenL, &sigGenR, buffer, 480);
+    sig_gen_output_combine(&sigGenL, &sigGenR, buffer, SAMPLES_NUM);
     ESP_LOG_BUFFER_HEX("buffer", buffer, BUF_SZ);
 
     while(1)
